2c41d66e16ec8f34ae0b9025325c54a853e09e93
kjtwagner <47577724+kjtwagner@users.noreply.github.com>
2021-07-27 10:47:36 -0400

    condense

--- 18/solution.py
+++ 18/solution.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
-import numpy as np
-
-char_count = 0
 text = input("Enter some text to get character count: ")
 
 ## take input and find the num char
-for i in text:
-    char_count += 1
-words = text.split(" ")
-word_count = len(words)
+char_count = 0
+for i in text: char_count += 1
+word_count = len(text.split(" "))
+
 ## return the num and print it for the user
 print("Char count: %i" % char_count)
 print("Word count: %i" % word_count)
