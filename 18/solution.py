0570d0705bf3c10964e891af166dcf0a9349973c
kjtwagner <47577724+kjtwagner@users.noreply.github.com>
2021-07-27 10:36:33 -0400

    added char count to sol

--- 18/solution.py
+++ 18/solution.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
 import numpy as np
+
+char_count = 0
+word_count = 0
+text = input("Enter some text to get character count: ")
+
+## take input and find the num char
+for i in text:
+    char_count += 1
+
+## return the num and print it for the user
+print(char_count)
