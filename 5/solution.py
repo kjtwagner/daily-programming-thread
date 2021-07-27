3078906853e1a8623010a2f318357499e2601b69
kjtwagner <47577724+kjtwagner@users.noreply.github.com>
2021-07-27 11:31:11 -0400

    remove list storage

--- 5/solution.py
+++ 5/solution.py
@@ -2,24 +2,17 @@
 
 import numpy as np
 
-# fizz buzz
-nums = np.arange(1,101,1)
-nums = list(nums)
-
 # loop through and replace
 i = 0
-for i in range(len(nums)):
+for i in range(1,101):
     # divisible by 3 is Fizz
-    if nums[i] % 3 == 0:
-        nums[i] = "Fizz"
+    if i  % 3 == 0:
+        print("Fizz")
         i+=1
     # divisible by 5 is Buzz
-    elif nums[i] % 5 == 0:
-        nums[i] = "Buzz"
+    elif i  % 5 == 0:
+        print("Buzz")
     # if neither, leave num alone
     else:
+        print(i)
         i += 1
-        
-# print final answer
-print(nums)
-
