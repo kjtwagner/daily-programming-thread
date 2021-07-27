32479f4a7a90583c88bc81c95cf5a8944f4caded
kjtwagner <47577724+kjtwagner@users.noreply.github.com>
2021-07-27 10:40:42 -0400

    added word counter

--- 18/solution.py
+++ 18/solution.py
@@ -3,12 +3,13 @@
 import numpy as np
 
 char_count = 0
-word_count = 0
 text = input("Enter some text to get character count: ")
 
 ## take input and find the num char
 for i in text:
     char_count += 1
-
+words = text.split(" ")
+word_count = len(words)
 ## return the num and print it for the user
-print(char_count)
+print("Char count: %i" % char_count)
+print("Word count: %i" % word_count)
